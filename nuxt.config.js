43a7a98c45b14cd0e828a8a3da2bd56795e10d64
浜口綾 <aya.hamaguchi@consallink.co.jp>
2021-07-09 15:15:38 +0900

    変更なし

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,10 +1,6 @@
 import colors from 'vuetify/es5/util/colors'
 
 export default {
-  generate: {
-    dir: 'public',
-  },
-
   // Disable server-side rendering: https://go.nuxtjs.dev/ssr-mode
   ssr: false,
 
