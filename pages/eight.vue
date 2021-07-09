28b92d265229ea2c1cb183c880e8e9b3f0dd88d0
浜口綾 <aya.hamaguchi@consallink.co.jp>
2021-07-09 15:16:22 +0900

    computedで詰まっている

--- pages/eight.vue
+++ pages/eight.vue
@@ -2,20 +2,50 @@
   <div>
     <span>激安！{{ taxExclud }}円</span><span>（税込み{{ taxInclud }}円）</span>
     <div>
-      <v-data-table :headers="headers"></v-data-table>
+      <v-simple-table>
+        <thead>
+          <tr>
+            <th>商品名</th>
+            <th>税抜き</th>
+            <th>税込み</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="(item, index) in items" :key="index">
+            <td>{{ item.name }}</td>
+            <td>{{ item.exclud }}</td>
+            <td>{{ includ }}</td>
+          </tr>
+        </tbody>
+      </v-simple-table>
     </div>
   </div>
 </template>
 <script lang="ts">
-import { defineComponent, ref } from '@vue/composition-api'
+import { computed, defineComponent, ref } from '@vue/composition-api'
 
 export default defineComponent({
   setup() {
     const taxExclud = ref(1000)
     const taxInclud = taxExclud.value * 1.1
+    const items = ref([
+      {
+        name: 'apple',
+        exclud: 100,
+      },
+      {
+        name: 'peach',
+        exclud: 200,
+      },
+    ])
+
+    const includ = computed((items: { exclud: string }) => items.exclud * 1.1)
+
     return {
       taxExclud,
       taxInclud,
+      items,
+      includ,
     }
   },
 })
